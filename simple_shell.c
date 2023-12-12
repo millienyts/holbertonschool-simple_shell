31ebf2cdf1fdfae897dbaa9ba7288fc0e747c21e
juancollazo5 <juancollazo577@outlook.com>
2023-12-12 15:01:21 +0000

    prototype functions update
    Fix and add display a prompt function, with simple comand lines, no semicolons, no pipes, no redirections or any other advanced features

--- simple_shell.c
+++ simple_shell.c
@@ -1,1 +1,64 @@
+#include "shell.h"
 
+int main(void) 
+{
+  char line[MAX_LINE_LENGTH];
+  char *command;
+  extern char **environ;
+
+  pid_t pid;
+
+  while (1) 
+{
+    /* Display prompt */
+    printf("> ");
+
+    /* Read user input */
+    if (fgets(line, MAX_LINE_LENGTH, stdin) == NULL) 
+	{
+      /* Handle end of file (Ctrl+D) */
+      printf("\n");
+      break;
+	}
+
+    /* Extract command */
+    line[strcspn(line, "\n")] = '\0';
+    command = strtok(line, " ");
+
+    /* Check if command is empty */
+    if (command == NULL) 
+	{
+      continue;
+	}
+
+    /* Check if executable exists */
+    if (access(command, X_OK) == -1) 
+	{
+      printf("'%s': command not found\n", command);
+      continue;
+	}
+
+    /* Fork a child process */
+    pid = 0;
+
+    if (pid == -1)
+	{
+      perror("fork");
+      exit(1);
+	}
+
+    /* Child process */
+    if (pid == 0)
+	{
+      /* Execute command */
+      execve(command, environ, NULL);
+      perror("execve");
+      exit(1);
+	}
+
+    /* Parent process*/
+    waitpid(pid, NULL, 0);
+}
+
+  return 0;
+}
