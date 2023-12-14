5a8532fed748568a3ad3e0618feb6d859036d67e
millienytorressanta <tmillieny@gmail.com>
2023-12-13 21:07:54 -0600

    simple shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,122 +1,108 @@
 #include "shell.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-char **parse_command(char *line);
+extern char **environ;
 
-void exit_shell() {
-    printf("Exiting shell.\n");
-    exit(EXIT_SUCCESS);
+#define MAX_ARGS 100
+
+void print_prompt(int interactive) {
+    if (interactive) {
+        char *dollar = "$";
+        printf("%s ", dollar);
+    }
 }
 
 int main(void) {
-    char line[MAX_LINE_LENGTH];
-    char **args;
-    char *copied_file;
+    int num_tokens, k;
+    size_t i;
+    char *args[MAX_ARGS];
+    char *command = NULL;
+    char *token;
+    size_t buffer = 0;
+    size_t length;
+    char *path[] = {"/usr/local/bin/", "/usr/bin/", "/bin/", "/usr/local/games/", "/usr/games/"};
+    char full_path[MAX_ARGS];
+    int status;
     pid_t pid;
+    char **env = environ;
 
-    while (1) {
-        /* Display prompt */
-        printf(":) ");
+    int interactive = isatty(fileno(stdin));
 
-        /* Read user input */
-        if (fgets(line, MAX_LINE_LENGTH, stdin) == NULL) {
-            /* Handle end of file (Ctrl+D) */
+    	while (1) {
+        print_prompt(interactive);
+        if (getline(&command, &buffer, stdin) != -1) {
+            length = strlen(command);
+            if (command[length - 1] == '\n')
+                command[length - 1] = '\0';
+
+            if (strcmp(command, "") == 0)
+                continue;
+
+            if (strcmp(command, "exit") == 0) {
+                free(command);
+                return 0;
+            }
+
+            num_tokens = 0;
+            token = strtok(command, " ");
+            while (token != NULL && num_tokens < MAX_ARGS - 1) {
+                args[num_tokens++] = token;
+                token = strtok(NULL, " ");
+            }
+            args[num_tokens] = NULL;
+
+            if (num_tokens == 0)
+                continue;
+
+            for (k = 0; k < num_tokens; k++)
+                ;
+
+            if (access(args[0], X_OK) == 0) {
+                for (i = 0; i < 5; i++) {
+                    execve(command, args, env);
+                    break;
+                }
+
+            } else {
+                for (i = 0; i < 5; i++) {
+                    strcpy(full_path, path[i]);
+                    strcat(full_path, args[0]);
+
+                    if (access(full_path, X_OK) == 0)
+                        break;
+                }
+            }
+
+            if (access(full_path, X_OK) == -1) {
+                printf("Command not found\n");
+                continue;
+            }
+            pid = fork();
+            if (pid == -1) {
+                perror("fork failed");
+                exit(EXIT_FAILURE);
+            }
+
+            if (pid == 0) {
+                if (execve(full_path, args, env) == -1) {
+                    printf("Command not found\n");
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                waitpid(pid, &status, 0);
+            }
+        } else {
+            if (!interactive)
+                break;
             printf("\n");
             break;
         }
-
-        line[strcspn(line, "\n")] = '\0'; /* Remove trailing newline */
-
-        if (line[0] == '\0' || line[0] == ' ') {
-            /* Case: Empty or spaces-only line */
-            continue;
-        }
-
-        /* Parse command */
-        args = parse_command(line);
-
-        /* Check for exit command */
-        if (!strcmp(args[0], "exit")) {
-            free(args);
-            break;
-        }
-	
-	/* Handle special case: copy and execute */
-	if (!strcmp(args[0], "cp")) {
-    	pid_t cp_pid;
-
-    	if (args[1] == NULL || args[2] == NULL) {
-        fprintf(stderr, "Invalid cp command.\n");
-        free(args);
-        continue;
     }
-
-    /* Copy the file using cp */
-    cp_pid = fork();
-    if (cp_pid == 0) {
-        execlp("cp", "cp", args[1], args[2], (char *)NULL);
-        perror("cp");
-        exit(EXIT_FAILURE);
-    }
-    waitpid(cp_pid, NULL, 0);
-
-    /* Extract copied filename and remaining arguments */
-    copied_file = strdup(args[2]);
-    args[0] = copied_file;
-}
-
-        /* Fork a child process */
-        pid = fork();
-
-        if (pid == 0) { /* Child process */
-            /* Execute command with arguments */
-            execvp(args[0], args);
-            perror(args[0]);
-            free(args[0]);
-            free(args);
-            exit(EXIT_FAILURE);
-        } else if (pid == -1) { /* Error */
-            perror("fork");
-            free(args);
-            exit(EXIT_FAILURE);
-        } else { /* Parent process */
-            waitpid(pid, NULL, 0); /* Wait for child to finish */
-            free(args[0]);
-            free(args);
-        }
-    }
-
+    free(command);
     return 0;
-}
-
-/* Implementation of parse_command function */
-char **parse_command(char *line) {
-    int buffer_size = MAX_LINE_LENGTH;
-    int position = 0;
-    char **tokens = malloc(buffer_size * sizeof(char *));
-    char *token;
-
-    if (!tokens) {
-        fprintf(stderr, "Allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    token = strtok(line, " ");
-    while (token != NULL) {
-        tokens[position] = token;
-        position++;
-
-        if (position >= buffer_size) {
-            buffer_size += MAX_LINE_LENGTH;
-            tokens = realloc(tokens, buffer_size * sizeof(char *));
-            if (!tokens) {
-                fprintf(stderr, "Allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        token = strtok(NULL, " ");
-    }
-    tokens[position] = NULL;
-    return tokens;
 }
