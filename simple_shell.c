58f7e4a1d26d48059b117eebaf408227be457070
millienytorressanta <tmillieny@gmail.com>
2023-12-13 20:28:22 -0600

    simple shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,20 +1,25 @@
 #include "shell.h"
+#include <stdio.h>
 
-int main(void)
-{
+char **parse_command(char *line);
+
+void exit_shell() {
+    printf("Exiting shell.\n");
+    exit(EXIT_SUCCESS);
+}
+
+int main(void) {
     char line[MAX_LINE_LENGTH];
     char **args;
     char *copied_file;
     pid_t pid;
 
-    while (1)
-    {
+    while (1) {
         /* Display prompt */
-        printf("> ");
+        printf(":) ");
 
         /* Read user input */
-        if (fgets(line, MAX_LINE_LENGTH, stdin) == NULL)
-        {
+        if (fgets(line, MAX_LINE_LENGTH, stdin) == NULL) {
             /* Handle end of file (Ctrl+D) */
             printf("\n");
             break;
@@ -22,8 +27,8 @@ int main(void)
 
         line[strcspn(line, "\n")] = '\0'; /* Remove trailing newline */
 
-        if (!strcmp(line, ""))
-        { /* Case: Spaces only (small/large/medium) */
+        if (line[0] == '\0' || line[0] == ' ') {
+            /* Case: Empty or spaces-only line */
             continue;
         }
 
@@ -31,18 +36,15 @@ int main(void)
         args = parse_command(line);
 
         /* Check for exit command */
-        if (!strcmp(args[0], "exit"))
-        {
+        if (!strcmp(args[0], "exit")) {
             free(args);
             break;
         }
 
         /* Handle special case: copy and execute */
-        if (!strcmp(args[0], "cp"))
-        {
+        if (!strcmp(args[0], "cp")) {
             int ret = system(line); /* Copy using system() */
-            if (ret != 0)
-            {
+            if (ret != 0) {
                 perror("system");
                 free(args);
                 continue;
@@ -51,28 +53,23 @@ int main(void)
             /* Extract copied filename and remaining arguments */
             copied_file = strtok(line + 3, " ");
             args[0] = strdup(copied_file);
-	    free(copied_file);
+            free(copied_file);
         }
 
         /* Fork a child process */
         pid = fork();
 
-        if (pid == 0)
-        { /* Child process */
+        if (pid == 0) { /* Child process */
             /* Execute command with arguments */
             execvp(args[0], args);
             perror(args[0]);
             free(args);
             exit(1);
-        }
-        else if (pid == -1)
-        { /* Error */
+        } else if (pid == -1) { /* Error */
             perror("fork");
             free(args);
             exit(1);
-        }
-        else
-        { /* Parent process */
+        } else { /* Parent process */
             waitpid(pid, NULL, 0); /* Wait for child to finish */
             free(args);
         }
@@ -82,31 +79,26 @@ int main(void)
 }
 
 /* Implementation of parse_command function */
-char **parse_command(char *line)
-{
+char **parse_command(char *line) {
     int buffer_size = MAX_LINE_LENGTH;
     int position = 0;
     char **tokens = malloc(buffer_size * sizeof(char *));
     char *token;
 
-    if (!tokens)
-    {
+    if (!tokens) {
         fprintf(stderr, "Allocation error\n");
         exit(EXIT_FAILURE);
     }
 
     token = strtok(line, " ");
-    while (token != NULL)
-    {
+    while (token != NULL) {
         tokens[position] = token;
         position++;
 
-        if (position >= buffer_size)
-        {
+        if (position >= buffer_size) {
             buffer_size += MAX_LINE_LENGTH;
             tokens = realloc(tokens, buffer_size * sizeof(char *));
-            if (!tokens)
-            {
+            if (!tokens) {
                 fprintf(stderr, "Allocation error\n");
                 exit(EXIT_FAILURE);
             }
